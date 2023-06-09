be119b32dc07fb84c0df1aefb45208176abe579a
Semih Erdogan <82077230+semiherd@users.noreply.github.com>
2023-06-09 20:24:33 +0300

    Update apiHeader.js

--- src/function/apiHeader.js
+++ src/function/apiHeader.js
@@ -1,11 +1,7 @@
 const ApiHeader={
 	'Accept': 'application/json',
-	'Authorization': 'Bearer re25vvfawyqgd5dghrdnucuq',
+	'Authorization': 'Bearer *****',
 	'Access-Control-Allow-Origin':'*',
 	'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
 }
 export default ApiHeader;
-
-//id wfvtqt5kx84c3uuz7b6ebusxw
-//secret cUQXrHkvrB
-//token 8jeck2j6dfbnqubhzftpbaam
